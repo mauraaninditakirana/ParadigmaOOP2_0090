3c5a78745f066958eab3dd18f01e4be8e9d26272
mauraaninditakirana <kiranamaura351@gmail.com>
2024-05-27 15:42:01 +0700

    membuat coding abstraksiklas dengan akses private dan public, dan membuat fungsi utama serta menjalankannya

---
+++ abstraksi.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+class AbstraksiKlas {
+private: string x, y;
+
+public:
+    //method untuk mengisi nilai
+    //private member
+    void setXY(string a, string b) {
+        x = a;
+        y = b;
+    }
+    //manampilkan nilai
+    void display() {
+        cout << "x= " << x << endl;
+        cout << "y= " << y << endl;
+    }
+};
+int main() {
+    AbstraksiKlas ak;
+    ak.setXY("Yogyakarta", "Kampus");
+    ak.display();
+
+    return 0;
+}
